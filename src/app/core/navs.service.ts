9861bd1432f37d1b6959672c4bc101b1705d8908
hirosi912 <hirosi912@users.noreply.github.com>
2023-06-19 13:55:07 +0800

    Create navs.service.ts

---
+++ src/app/core/navs.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../core.module';
+import { LocalStorageService } from 'src/app/core/local-storage/local-storage.service';
+// import { actionSettingsChangeNavs } from '../settings/settings.actions';
+
+export interface Nav {
+    label: string;
+    routerLink: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NavsService {
+    navs: Nav[] = [];
+    public navsChange = new BehaviorSubject(this.navs);
+    navsChange$ = this.navsChange.asObservable();
+
+    constructor(
+        private store: Store<AppState>,
+        private localStorageService: LocalStorageService,
+        private router: Router
+    ) {
+
+    }
+
+    setNavs(
+        snapshot: ActivatedRouteSnapshot,
+        lazyTranslateService?: TranslateService
+    ) {
+        let lastChild = snapshot;
+        while (lastChild.children.length) {
+            lastChild = lastChild.children[0];
+        }
+        this.navs = this.localStorageService.getItem('Navs') ? this.localStorageService.getItem('Navs') : [{
+            label: 'home',
+            routerLink: '/home'
+        }];
+        if (this.navs) {
+            let index = true;
+            this.navs.forEach(item => {
+                if (item.routerLink === this.router.url.split('?')[0]) {
+                    index = false;
+                }
+            });
+
+            if (index) {
+                this.navs.push({
+                    label: lastChild.data['title'],
+                    routerLink: this.router.url
+                });
+            }
+            this.localStorageService.setItem('Navs', this.navs);
+            // this.store.dispatch(actionSettingsChangeNavs({ navs: this.navs }));
+        }
+        this.navsChange.next(this.navs);
+        // this.store.dispatch(actionSettingsChangeNavs({ navs: this.navs }));
+    }
+}
