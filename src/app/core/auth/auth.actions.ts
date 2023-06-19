5d5e075caf5305b5c0090a4eb04f58ca51338889
hirosi912 <hirosi912@users.noreply.github.com>
2023-06-19 13:46:09 +0800

    Create auth.actions.ts

---
+++ src/app/core/auth/auth.actions.ts
@@ -0,0 +1,40 @@
+// import { createAction, props } from '@ngrx/store';
+// import { UserInfo } from './auth.models';
+
+// export const initAuthFlow = createAction('[Auth] Init auth flow');
+
+// export const listenAuthEvents = createAction('[Auth] Listen auth events');
+
+// export const login = createAction('[Auth] Login');
+
+// export const loginSuccess = createAction('[Auth] Login success');
+
+// export const logout = createAction('[Auth] Logout');
+
+// export const logoutSuccess = createAction('[Auth] Logout success');
+
+// export const loadUserInfo = createAction('[Auth] Load user info');
+
+// export const loadUserInfoSuccess = createAction(
+//   '[Auth] Load user info success',
+//   props<{ userInfo: UserInfo }>()
+// );
+
+// export const loadUserClaims = createAction('[Auth] Load user claims');
+
+// export const loadUserClaimsSuccess = createAction(
+//   '[Auth] Load user claims success',
+//   props<{ claims: any }>()
+// );
+
+// export const loadPermissions = createAction('[Permission] Load Permissions');
+
+// export const loadPermissionsSuccess = createAction(
+//   '[Permission] Load Permissions Success',
+//   props<{ data: any }>()
+// );
+
+// export const loadPermissionsFail = createAction(
+//   '[Permission] Load Permissions Fail',
+//   props<{ error: string }>()
+// );
